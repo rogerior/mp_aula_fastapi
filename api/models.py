3842dce04f6df2363d5abd04ed8b3a52ed4d5aea
Rogério Rodrigues <rogerionrc@gmail.com>
2026-05-14 10:22:57 -0300

    teste da action do pre-commit

--- api/models.py
+++ api/models.py
@@ -5,22 +5,14 @@ from enum import Enum
 class Numeros(BaseModel):
     numero1: int
     numero2: int
-
-
 class Resultado(BaseModel):
     resultado: int
-
-
 class TipoOperacao(str, Enum):
     soma = "soma"
     subtracao = "subtracao"
     multiplicacao = "multiplicacao"
     divisao = "divisao"
-
-
 class HistoriaInput(BaseModel):
     tema: str
-
-
 class HistoriaOutput(BaseModel):
     historia: str
